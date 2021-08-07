1c2bad97e70b0a4e32ee01fa620a87c63d6a9f33
clarissaorrico2 <88287071+clarissaorrico2@users.noreply.github.com>
2021-08-07 11:24:31 +0200

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,7 +1,20 @@
 
 echo "$(<script)"
-My name: Clarissa
-email: issa.93@live.it
-slackusername:@clarissaorrico
-biostack:Genomics
+name: Clarissa,
+email: issa.93@live.it,
+slackusername:@clarissaorrico,
+biostack:Genomics,
+twitter:@ClarissaOrrico,
+hamming distance
+
+str1=@clarissaorrico
+str2=@ClarissaOrrico
+
+def hamming distance (str1, str2):
+    L=len (str1)
+    for i in range (L):
+	if str[i] != str2[i]:
+ 	   distance +=0
+	   return distance 
+	   print (hamming_Dist (str1,str2))
 
